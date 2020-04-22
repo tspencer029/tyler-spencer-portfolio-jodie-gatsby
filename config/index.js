25dfb61e932ca2ac1f87cc18135d1d39d0c3aac6
Tyler Spencer <tspencer029@gmail.com>
2020-04-22 22:24:55 +1200

    updated config

--- config/index.js
+++ config/index.js
@@ -1,19 +1,19 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Jodie', // Navigation and Site Title
-  siteTitleAlt: 'Jodie - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Jodie', // short_name for manifest
-  siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://jodie.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Tyler Spencer', // Navigation and Site Title
+  siteTitleAlt: 'Tyler Spencer photography and development portfolio', // Alternative Site title for SEO
+  siteTitleShort: 'Tyler Spencer', // short_name for manifest
+  siteHeadline: 'Contact me for all your photography and development needs', // Headline for schema.org JSONLD
+  siteUrl: 'https://tylerspencer.nz', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
-  siteDescription: 'Image-heavy photography portfolio with colorful accents & great typography',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteDescription: 'Photography and development portfolio for Tyler Spencer',
+  author: 'Tyler Spencer', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@jodie', // Twitter Username
-  ogSiteName: 'jodie', // Facebook Site Name
+  userTwitter: '@tspencer029', // Twitter Username
+  ogSiteName: 'TDJ Photography', // Facebook Site Name
   ogLanguage: 'en_US', // og:language
   googleAnalyticsID: 'UA-XXXXXX-X',
 
