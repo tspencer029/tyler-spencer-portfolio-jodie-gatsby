33f6148822a9fb896e2f7cc5ab7536c3e48de096
Tyler Spencer <tspencer@enz.lan>
2020-05-07 11:15:22 +1200

    logo test 3

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Logo = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 493.19 493.19">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="-40 -40 80 80">
     <title>LekoArts</title>
     <circle r="39"/>
 	<path d="M0,38a38,38 0 0 1 0,-76a19,19 0 0 1 0,38a19,19 0 0 0 0,38" fill="#fff"/>
