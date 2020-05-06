8098b190577381174c71ea3328a14199c6a73567
Tyler Spencer <tspencer@enz.lan>
2020-05-07 11:51:28 +1200

    test logo new

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,12 +2,36 @@ import React from 'react'
 
 const Logo = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="-40 -40 80 80">
-    <title>LekoArts</title>
-    <circle r="39"/>
-	<path d="M0,38a38,38 0 0 1 0,-76a19,19 0 0 1 0,38a19,19 0 0 0 0,38" fill="#fff"/>
-	<circle cy="19" r="5" fill="#fff"/>
-	<circle cy="-19" r="5" />
-  </svg>
+    <title>Tyler Spencer</title>
+    <defs>
+    <style>
+      .cls-1 {
+        fill: none;
+      }
+
+      .cls-1, .cls-2 {
+        // stroke: #000;
+        stroke-width: 10px;
+      }
+
+      .cls-2 {
+        fill-rule: evenodd;
+      }
+
+      .cls-3, .cls-4 {
+        font-size: 416.667px;
+        font-family: Arial;
+      }
+
+      .cls-3 {
+        // fill: #fff;
+      }
+    </style>
+  </defs>
+  <rect class="cls-1" x="62" y="62" width="1058" height="1058"/>
+  <path id="Polygon_1" data-name="Polygon 1" class="cls-2" d="M1120.54,1117.06L62.629,1118.49V60.51Z"/>
+  <text id="T" class="cls-3" x="237.647" y="987.753">T</text>
+  <text id="S" class="cls-4" x="704.353" y="514.637">S</text>
 )
 
 export default Logo
