c4f40f49d2ec1f0ca64a9783be8e196f0a41f54e
Tyler Spencer <tspencer@enz.lan>
2020-05-07 12:00:50 +1200

    test logo new 3

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,38 +1,30 @@
 import React from 'react'
 
 const Logo = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="100.031" height="100.031" viewBox="0 0 1181 1181">
-  <defs>
-    <style>
-      .cls-1 {
-        fill: none;
-      }
-
-      .cls-1, .cls-2 {
-        stroke: #000;
-        stroke-width: 10px;
-      }
-
-      .cls-2 {
-        fill-rule: evenodd;
-      }
-
-      .cls-3, .cls-4 {
-        font-size: 416.667px;
-        font-family: Arial;
-      }
-
-      .cls-3 {
-        fill: #fff;
-      }
-    </style>
-  </defs>
-  <rect class="cls-1" x="62" y="62" width="1058" height="1058"/>
-  <path id="Polygon_1" data-name="Polygon 1" class="cls-2" d="M1120.54,1117.06L62.629,1118.49V60.51Z"/>
-  <text id="T" class="cls-3" x="237.647" y="987.753">T</text>
-  <text id="S" class="cls-4" x="704.353" y="514.637">S</text>
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve"> <title>Tyler Spencer</title>  <image id="image0" width="100" height="100" x="0" y="0"
+    xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAQAAADa613fAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElN
+RQfkBQcCOh7IFHOJAAAD5klEQVR42u3bb4gTRxjH8W+MPfVavYv/rdY/J6LYF7YeiKCUgiKVFoTi
+FQRBELmCIggtLoLgC6k9sSKiIBEVUakSbTmlxcOLJIgFwfONoCKSQl/2zfnKd9XHF8ldE29vdyeZ
+ySaP+c2bI5udzOdmd2Z2k4VWWmnlvUii7O//+Cfu5hhmAZP8XhZ+irtlhnns/7Ig/Bx322xBhGNx
+t84WRDged/tsQYSTFYNA4yYUIpxqCkoEiHCmCSiRIEK64SkRIcI5JsTdVjsQ4QLJuFtrByJcamCK
+EUS40rAUQ4hw1THFI10xyHiuIEKGic4Q/p+YdgMRbtDmgDEY8IlDpFxAhN+tU/pKNQ9WHEzeKC/j
+BiLctEpJlWr1OyNGDriNbiDCHxYpPQhCYZytxV7piwoxnbm/5iaTLUG6AMiOs/U6AN3VVBylRwRh
+gClWIF7plK4+NRxaxXKHdguQ7oBzpE4Q4a4VSmZ0oPVCh1pHECHPhzVDUhQq6uzDMzorrECEe3xk
+gZLxqTkT8XCzBBHuM61mCnThvdMzxdJbP4jwFx0WKMV4Y0Bh6y2LEOGBRUoxvaNLl7ARzSpEeEin
+ZQqkSjP7cD0hwiOmV/F/D54OUwwjBK+2alii+Gc1WWYY7vM3MLJM8ctLHoW8oyK27pJ8TpZZRnsU
+m5kKGJtSJZBxqj+0iuWxIaU4gxTGmdM3lmoN6hHr58hIecIcA8jIWmvIZzbvLg3Eg4E1OIMIT5lr
+QPl/mE2XHWI9ZbcighcsDiHCMz42oKQD6+oJ2dspRHjOfANKb2mYfbcMRVg+OoYIL/jEgAK9eGWc
+Al7ItXrdIEKBhUaU6mJ9QhybLvIsrgPFOQSWkGeJBggsIs9SDRBYSJ5lGiCwgBzLNUBgPjlWaIDA
+PHKs1ACBueT4VAMEZpNjlQYIzCLLZxogMJMsqzVAYAbZ6r4oaDQIpMiyRgMEOrnD2hgheyKu3wfC
+q+pggHWxQaymg9us1wCBqdzmC1eVG14F/isiIserv4p8xZc1trgOV4hR0s6fbLBVWbw/LGvnFps0
+QKCdfr7SAIEp9LNZAwQm0c83GiDQxm9s0QCBNjJ8qwECbVxjqwYIfMBVvtMAgYn8yjYNEEhyme0a
+IJDkIjs0QCDJeXZqgECSc+zSAIEEZ/leAwQSnGG3BggkOM1eDRBIcJJ9GiCQ4AQ/aIAA/MJ+HRA4
+ygGTt9v8nt1+OejT4ga5i2KWwxzS0SOCcFhDjwAc5Mh4m5oLAgc4qgMC+/2fuG8+CPzIibFPETcj
+BPaNfeK+OSGwl9OVFFePSbrP7spOKFeJUUVv6tDY1yHbJzRxR7TSSivved4CPT6wRRgOJL0AAAAl
+dEVYdGRhdGU6Y3JlYXRlADIwMjAtMDUtMDdUMDI6NTg6MzArMDM6MDBRCeLNAAAAJXRFWHRkYXRl
+Om1vZGlmeQAyMDIwLTA1LTA3VDAyOjU4OjMwKzAzOjAwIFRacQAAAABJRU5ErkJggg==" />
 </svg>
-
 )
 
 export default Logo
